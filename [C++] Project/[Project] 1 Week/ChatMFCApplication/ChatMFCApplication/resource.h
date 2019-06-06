fe349f8742ceea0d8bdb1f7ac00bdb4a6146e6f6
양창엽 <ChangYeop-Yang@users.noreply.github.com>
2019-06-07 08:12:07 +0900

    Create 2019.06.07. 08:08
    Create 2019.06.07. 08:08
    1st Project 생성

---
+++ [C++] Project/[Project] 1 Week/ChatMFCApplication/ChatMFCApplication/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ChatMFCApplication.rc
+//
+#define IDD_CHATMFCAPPLICATION_DIALOG   102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1000
+#define IDC_EDIT3                       1012
+#define IDC_SCROLLBAR1                  1016
+#define IDC_MFCBUTTON2                  1021
+#define IDC_MONTHCALENDAR1              1030
+#define IDC_MFCBUTTON3                  1031
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1032
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
